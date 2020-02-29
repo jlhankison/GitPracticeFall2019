a2fc446b9aadc89cb8be2cbbe7270f2f395b859a
jlhankison <jlhankison@gmail.com>
2020-02-29 14:25:48 -0600

    Second commit, another string out

--- src/Day01/StringPractice.java
+++ src/Day01/StringPractice.java
@@ -4,5 +4,9 @@ public class StringPractice {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        String str = "Java is fun";
+
+        System.out.println(str);
     }
 }
